52cf07f2c7889110023dcca681def472a07c52a4
Myron <dubtwee@gmail.com>
2019-08-19 10:13:27 -0500

    fleshing out render function for View UI

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -30,5 +30,23 @@ function getTasks() {
 
 function render(tasksArray) {
     console.log(tasksArray);
+    const $tasksList = $('.js-tasks-list');
+
+    $tasksList.empty()
+    for (let task of tasksArray) {
+        $tasksList.append(`
+            <li class="list-group-item">
+                <div class="row">
+                    <div class="col-6">
+                        ${task.description}
+                    </div>
+                    <div class="col-6 text-right">
+                        <button class="btn btn-success">Complete</button>
+                        <button class="btn btn-danger">Delete</button>
+                    </div>
+                </div>
+            </li>
+        `);
+    }
 
 }
